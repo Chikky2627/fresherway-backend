4efa2a885a1c5b24b8bb88c7f44c14e2c1d24baa
chikky_pathuri <pathurichikkyreddy@gmail.com>
2026-06-11 21:53:33 +0530

    Day 1: Project setup and database connection

---
+++ OneDrive/FresherWay/fresherway/src/test/java/com/fresherway/fresherway/FresherwayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fresherway.fresherway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FresherwayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
